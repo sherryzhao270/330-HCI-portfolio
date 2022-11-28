a7855ff27a172bef1a2b8d1085dc434438bd76bb
Sherry Zhao <sherrytxz06@gmail.com>
2022-11-28 02:23:06 -0600

    projects and projects detail added

---
+++ home.js
@@ -0,0 +1,9 @@
+const openModal = (id) => {
+    let modal = document.getElementById(id);
+    modal.style.display = "block";
+}
+
+const closeModal = (id) => {
+    let modal = document.getElementById(id);
+    modal.style.display = "none";
+}

ef32ae8b4663c8846f6cbcf33fc7d804a82d76f8
Sherry Zhao <sherrytxz06@gmail.com>
2022-12-01 15:14:26 -0600

    Add link to phone; style links

--- userProfile.js
+++ userProfile.js
@@ -4,7 +4,7 @@ const displayUserProfile = () => {
     let profile = document.getElementById("user-profile-content")
     profile.innerHTML = `<div> Username: ${currentUser.username} </div>
                         <div> Email: <a href="mailto:${currentUser.email}">${currentUser.email}</a> </div>
-                        <div> Phone: ${currentUser.phone} </div>`
+                        <div> Phone: <a href="tel:${currentUser.phone}">${currentUser.phone}</a> </div>`
 }
 
 displayUserProfile()

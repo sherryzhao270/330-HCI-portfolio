65b548f9ae8d4781975e678a0e03b00f783422d7
Sherry Zhao <sherrytxz06@gmail.com>
2022-12-02 03:22:06 -0600

    Fixed bug: Open project btn after logout

--- project.js
+++ project.js
@@ -4,11 +4,11 @@ const projectButton = () => {
     let projects = document.getElementsByClassName("project-detail");
     
     if (projects.length > 0) {
-        currentUser.accountType === "advanced"
-            ? (projects[0].innerHTML += `<a href="http://www.responsinator.com/?url=intune-ab424.web.app%2F"><button> Open Project </button></a>`,
-                projects[1].innerHTML += `<a href="https://cs333-fianlproj-group1.herokuapp.com/"><button> Open Project </button></a>`)
-            : (projects[0].innerHTML += `<button onclick="advanceAccountNeeded()"> Open Project </button>`,
-                projects[1].innerHTML += `<button onclick="advanceAccountNeeded()"> Open Project </button>`)
+        currentUser && currentUser.accountType === "advanced"
+            ? (document.getElementById("project1-btn").innerHTML = `<a href="http://www.responsinator.com/?url=intune-ab424.web.app%2F"><button> Open Project </button></a>`,
+                document.getElementById("project2-btn").innerHTML = `<a href="https://cs333-fianlproj-group1.herokuapp.com/"><button> Open Project </button></a>`)
+            : (document.getElementById("project1-btn").innerHTML = `<button onclick="advanceAccountNeeded()"> Open Project </button>`,
+                document.getElementById("project2-btn").innerHTML = `<button onclick="advanceAccountNeeded()"> Open Project </button>`)
     }
     
 }
